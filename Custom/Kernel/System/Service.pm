0cf4534075fdf9a9a0894d144331d96a0c30cc49
StefanRother-OTOBO <sr@rother-oss.com>
2023-08-24 14:53:07 +0200

    Initial release OTOBO 11.

--- Custom/Kernel/System/Service.pm
+++ Custom/Kernel/System/Service.pm
@@ -2,9 +2,9 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2021 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
 # --
-# $origin: otobo itsm core - 734da46f4d87076e659f043ed406e5d91767f7b0 - Kernel/System/Service.pm
+# $origin: otobo - fa038a38019d88902d7e5fddf3dcdfeb2effbbf0 - Kernel/System/Service.pm
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
--- var/httpd/htdocs/js/Core.UI.Dialog.js
+++ var/httpd/htdocs/js/Core.UI.Dialog.js
@@ -2,7 +2,9 @@
 // OTOBO is a web-based ticketing system for service organisations.
 // --
 // Copyright (C) 2001-2020 OTRS AG, https://otrs.com/
-// Copyright (C) 2019-2022 Rother OSS GmbH, https://otobo.de/
+// Copyright (C) 2019-2023 Rother OSS GmbH, https://otobo.de/
+// --
+// $origin: otobo - a077e914380d1a13d5aa31472ea687353b614622 - var/httpd/htdocs/js/Core.UI.Dialog.js
 // --
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
